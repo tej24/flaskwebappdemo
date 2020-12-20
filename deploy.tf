5a3379bae65fd81c8f3058222cb03548f8bfb6fb
Thejus R A <thejus55@gmail.com>
2020-12-20 10:54:03 +0530

    Added terraform deploy file

---
+++ deploy.tf
@@ -0,0 +1,146 @@
+# Configure the Azure provider
+
+terraform {
+  required_version = "~> 0.14.2"
+  required_providers {
+    azurerm = "=2.41.0"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+#provider "azurerm" {
+#    # The "feature" block is required for AzureRM provider 2.x.
+#    # If you are using version 1.x, the "features" block is not allowed.
+#    version = "~> 2.21.0"
+#    features {}
+#}
+
+resource "azurerm_resource_group" "flasktestrg" {
+    name = "flasktestResourceGroup"
+    location = "westus2"
+}
+
+resource "azurerm_app_service_plan" "flasktest" {
+    name                = "flasktestAppServicePlan"
+    location            = azurerm_resource_group.flasktestrg.location
+    resource_group_name = azurerm_resource_group.flasktestrg.name
+
+    # Define Linux as Host OS
+    kind = "Linux"
+    reserved = true
+
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+
+}
+
+
+# Create an Azure Web App for Containers in that App Service Plan
+resource "azurerm_app_service" "dockerapp" {
+  name                = "flasktest-dockerapp"
+  location            = azurerm_resource_group.flasktestrg.location
+  resource_group_name = azurerm_resource_group.flasktestrg.name
+  app_service_plan_id = azurerm_app_service_plan.flasktest.id
+
+  # Do not attach Storage by default
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+
+
+    # Settings for private Container Registires
+    DOCKER_REGISTRY_SERVER_URL      = "https://index.docker.io"
+    DOCKER_REGISTRY_SERVER_USERNAME = "thejus55"
+    DOCKER_REGISTRY_SERVER_PASSWORD = "dockthej*2020"
+
+  }
+
+  # Configure Docker Image to load on start
+  site_config {
+    linux_fx_version = "DOCKER|thejus55/flaskwebapp:1.0"
+    always_on        = "true"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+thejus@Azure:~/RSA/deploy$ cat deploy.tf
+# Configure the Azure provider
+
+terraform {
+  required_version = "~> 0.14.2"
+  required_providers {
+    azurerm = "=2.41.0"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+#provider "azurerm" {
+#    # The "feature" block is required for AzureRM provider 2.x.
+#    # If you are using version 1.x, the "features" block is not allowed.
+#    version = "~> 2.21.0"
+#    features {}
+#}
+
+resource "azurerm_resource_group" "flasktestrg" {
+    name = "flasktestResourceGroup"
+    location = "westus2"
+}
+
+resource "azurerm_app_service_plan" "flasktest" {
+    name                = "flasktestAppServicePlan"
+    location            = azurerm_resource_group.flasktestrg.location
+    resource_group_name = azurerm_resource_group.flasktestrg.name
+
+    # Define Linux as Host OS
+    kind = "Linux"
+    reserved = true
+
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+
+}
+
+
+# Create an Azure Web App for Containers in that App Service Plan
+resource "azurerm_app_service" "dockerapp" {
+  name                = "flasktest-dockerapp"
+  location            = azurerm_resource_group.flasktestrg.location
+  resource_group_name = azurerm_resource_group.flasktestrg.name
+  app_service_plan_id = azurerm_app_service_plan.flasktest.id
+
+  # Do not attach Storage by default
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+
+
+    # Settings for private Container Registires
+    DOCKER_REGISTRY_SERVER_URL      = "https://index.docker.io"
+    DOCKER_REGISTRY_SERVER_USERNAME = "thejus55"
+    DOCKER_REGISTRY_SERVER_PASSWORD = "dockthej*2020"
+
+  }
+
+  # Configure Docker Image to load on start
+  site_config {
+    linux_fx_version = "DOCKER|thejus55/flaskwebapp:1.0"
+    always_on        = "true"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
